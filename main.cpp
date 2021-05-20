4b27f562b35b8678e1b56d2059d07b9d0a893aca
Egor Vdovenko <yodaskillme@gmail.com>
2021-05-20 20:35:56 +0300

    Edits

--- main.cpp
+++ main.cpp
@@ -9,36 +9,37 @@
 GLUquadricObj* gobj;
 GLuint woodTextureId;
 
-float sphereDiameter = 1.0;
-float sphereCube = 0.125;
+const float SPHERE_D = 1.0;
+const float SPHERE_CUBE_SIZE = 0.125;
 
-float baseX = 7.5;
-float baseY = 0.75;
-float baseZ = 5.5;
-float baseDistance = 1.0;
+const float BASE_X = 7.5;
+const float BASE_Y = 0.75;
+const float BASE_Z = 5.5;
+const float BASE_DISTANCE = 1.0;
 
-float pipeRadius = 0.125;
-float pipeX = 6.5;
-float pipeY = 5.0;
-float pipeZ = 4.5;
+const float TUBE_X = 6.5;
+const float TUBE_Y = 5.0;
+const float TUBE_Z = 4.5;
+const float TUBE_RADIUS = 0.125;
 
-float cameraX = 0.0;
-float cameraY = 0.5;
-float cameraZ = 15.0;
+const float CAMERA_X = 0.0;
+const float CAMERA_Y = 0.5;
+const float CAMERA_Z = 15.0;
+
+const float WIRE_L = TUBE_Y - TUBE_RADIUS - BASE_DISTANCE - SPHERE_D - SPHERE_CUBE_SIZE / 2;
+
 float cameraRotationX = 5.0;
 float cameraRotationY = 45.0;
 float cameraRotationZ = 0.0;
 
-float wireLength = pipeY - pipeRadius - baseDistance - sphereDiameter - sphereCube / 2;
-
-float angle = 45.0;                          // Угол отклонения
-float freq = 1000 / 50;                      // Частота смены кадров (млс)
-float A = wireLength;                        // Амплитуда колебаний, максимальное отклонение груза (м)
-float T = 2 * M_PI * sqrt(wireLength / 9.8); // Период колебаний (с)
-float v = 1 / T;                             // Частота колебаний (Гц)
-float omega = 2 * M_PI * v;                  // Циклическая частота колебаний (рад/с)
-float fi0 = M_PI / 4;                        // Начальная фаза колебания (рад)
-float t = 0;                                 // Время (c)
+float angle = 45.0;                      // Угол отклонения
+float freq = 1000 / 50;                  // Частота смены кадров (млс)
+float A = WIRE_L;                        // Амплитуда колебаний, максимальное отклонение груза (м)
+float T = 2 * M_PI * sqrt(WIRE_L / 9.8); // Период колебаний (с)
+float v = 1 / T;                         // Частота колебаний (Гц)
+float omega = 2 * M_PI * v;              // Циклическая частота колебаний (рад/с)
+float fi0 = M_PI / 4;                    // Начальная фаза колебания (рад)
+float t = 0;                             // Время (c)
 
 GLuint loadTexture(Image* image) {
 	GLuint woodTextureId;
@@ -54,103 +55,99 @@ void drawSphere(float angle) {
 	glPushMatrix();
 
 	glRotatef(angle, 0.0f, 0.0f, 1.0f);
-	glTranslatef(0.0f, -wireLength, 0.0f);
+	glTranslatef(0.0f, -WIRE_L, 0.0f);
 
 	glColor3f(0.675f, 0.675f, 0.750f);
 
-	glutSolidCube(sphereCube);
+	glutSolidCube(SPHERE_CUBE_SIZE);
 
 	glPushMatrix();
-	glTranslatef(0.0f, -(sphereDiameter / 2), 0.0f);
-	glutSolidSphere(sphereDiameter / 2, 100, 100);
+
+	glTranslatef(0.0f, -(SPHERE_D / 2), 0.0f);
+	glutSolidSphere(SPHERE_D / 2, 100, 100);
+
 	glPopMatrix();
 
 	glRotatef(-angle, 0.0f, 0.0f, 1.0f);
 
-	float x = sin(angle * M_PI / 180) * wireLength;
-	float y = cos(angle * M_PI / 180) * wireLength;
-	float z = pipeZ / 2 - pipeRadius;
+	glPopMatrix();
+}
+
+void drawWire(float angle) {
+	glPushMatrix();
+
+	glRotatef(angle, 0.0f, 0.0f, 1.0f);
+	glTranslatef(0.0f, -WIRE_L, 0.0f);
+	glRotatef(-angle, 0.0f, 0.0f, 1.0f);
+
+	float x = sin(angle * M_PI / 180) * WIRE_L;
+	float y = cos(angle * M_PI / 180) * WIRE_L;
+	float z = TUBE_Z / 2 - TUBE_RADIUS;
 
 	glColor3f(1.0f, 1.0f, 1.0f);
 
 	glBegin(GL_LINES);
+
 	glVertex3f(0.0f, 0.0f, 0.0f);
 	glVertex3f(-x, y, -z);
 	glVertex3f(0.0f, 0.0f, 0.0f);
 	glVertex3f(-x, y, z);
+
 	glEnd();
 
 	glPopMatrix();
 }
 
-void drawSpheres() {
+void drawTubes() {
 	glPushMatrix();
-	glTranslatef(0.0f, pipeY - pipeRadius - baseDistance - sphereDiameter / 2.0f, 0.0f);
-
-	for (int i = 1; i <= 5; i++) {
-		glPushMatrix();
-		glTranslatef(-5 / 2.0f - sphereDiameter / 2.0f + i * sphereDiameter, 0.0f, 0.0f);
-
-		(i == 1 && angle < 0 || i == 5 && angle > 0)
-			? drawSphere(angle)
-			: drawSphere(0);
-
-		glPopMatrix();
-	}
-
-	glPopMatrix();
-}
-
-void drawPipes() {
-	glPushMatrix();
-	glTranslatef(0.0f, pipeY / 2 - sphereDiameter / 2 - baseDistance, 0.0f);
+	glTranslatef(0.0f, TUBE_Y / 2 - SPHERE_D / 2 - BASE_DISTANCE, 0.0f);
 
 	glColor3f(0.675f, 0.675f, 0.750f);
 
 	//Top Front
 	glPushMatrix();
-	glTranslatef(-pipeX / 2 + pipeRadius, pipeY / 2 - pipeRadius, pipeZ / 2 - pipeRadius);
+	glTranslatef(-TUBE_X / 2 + TUBE_RADIUS, TUBE_Y / 2 - TUBE_RADIUS, TUBE_Z / 2 - TUBE_RADIUS);
 	glRotatef(90.0f, 0.0f, 1.0f, 0.0f);
-	gluCylinder(gobj, pipeRadius, pipeRadius, pipeX - pipeRadius * 2.0, 50, 50);
+	gluCylinder(gobj, TUBE_RADIUS, TUBE_RADIUS, TUBE_X - TUBE_RADIUS * 2.0, 50, 50);
 	glPopMatrix();
 
 	//Top Rear
 	glPushMatrix();
-	glTranslatef(-pipeX / 2 + pipeRadius, pipeY / 2 - pipeRadius, -(pipeZ / 2 - pipeRadius));
+	glTranslatef(-TUBE_X / 2 + TUBE_RADIUS, TUBE_Y / 2 - TUBE_RADIUS, -(TUBE_Z / 2 - TUBE_RADIUS));
 	glRotatef(90.0f, 0.0f, 1.0f, 0.0f);
-	gluCylinder(gobj, pipeRadius, pipeRadius, pipeX - pipeRadius * 2.0, 50, 50);
+	gluCylinder(gobj, TUBE_RADIUS, TUBE_RADIUS, TUBE_X - TUBE_RADIUS * 2.0, 50, 50);
 	glPopMatrix();
 
 	//Left Front
 	glPushMatrix();
-	glTranslatef(-(pipeX / 2 - pipeRadius), pipeY / 2 - pipeRadius, pipeZ / 2 - pipeRadius);
+	glTranslatef(-(TUBE_X / 2 - TUBE_RADIUS), TUBE_Y / 2 - TUBE_RADIUS, TUBE_Z / 2 - TUBE_RADIUS);
 	glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
-	gluCylinder(gobj, pipeRadius, pipeRadius, GLdouble(pipeY) - pipeRadius, 50, 50);
-	glutSolidSphere(pipeRadius, 20, 20);
+	gluCylinder(gobj, TUBE_RADIUS, TUBE_RADIUS, GLdouble(TUBE_Y) - TUBE_RADIUS, 50, 50);
+	glutSolidSphere(TUBE_RADIUS, 50, 50);
 	glPopMatrix();
 
 	//Left Rear
 	glPushMatrix();
-	glTranslatef(-(pipeX / 2 - pipeRadius), pipeY / 2 - pipeRadius, -(pipeZ / 2 - pipeRadius));
+	glTranslatef(-(TUBE_X / 2 - TUBE_RADIUS), TUBE_Y / 2 - TUBE_RADIUS, -(TUBE_Z / 2 - TUBE_RADIUS));
 	glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
-	gluCylinder(gobj, pipeRadius, pipeRadius, GLdouble(pipeY) - pipeRadius, 50, 50);
-	glutSolidSphere(pipeRadius, 20, 20);
+	gluCylinder(gobj, TUBE_RADIUS, TUBE_RADIUS, GLdouble(TUBE_Y) - TUBE_RADIUS, 50, 50);
+	glutSolidSphere(TUBE_RADIUS, 50, 50);
 	glPopMatrix();
 
 	//Right Front
 	glPushMatrix();
-	glTranslatef((pipeX / 2 - pipeRadius), pipeY / 2 - pipeRadius, pipeZ / 2 - pipeRadius);
+	glTranslatef((TUBE_X / 2 - TUBE_RADIUS), TUBE_Y / 2 - TUBE_RADIUS, TUBE_Z / 2 - TUBE_RADIUS);
 	glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
-	gluCylinder(gobj, pipeRadius, pipeRadius, GLdouble(pipeY) - pipeRadius, 50, 50);
-	glutSolidSphere(pipeRadius, 20, 20);
+	gluCylinder(gobj, TUBE_RADIUS, TUBE_RADIUS, GLdouble(TUBE_Y) - TUBE_RADIUS, 50, 50);
+	glutSolidSphere(TUBE_RADIUS, 50, 50);
 	glPopMatrix();
 
 	//Right Rear
 	glPushMatrix();
-	glTranslatef((pipeX / 2 - pipeRadius), pipeY / 2 - pipeRadius, -(pipeZ / 2 - pipeRadius));
+	glTranslatef((TUBE_X / 2 - TUBE_RADIUS), TUBE_Y / 2 - TUBE_RADIUS, -(TUBE_Z / 2 - TUBE_RADIUS));
 	glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
-	gluCylinder(gobj, pipeRadius, pipeRadius, GLdouble(pipeY) - pipeRadius, 50, 50);
-	glutSolidSphere(pipeRadius, 20, 20);
+	gluCylinder(gobj, TUBE_RADIUS, TUBE_RADIUS, GLdouble(TUBE_Y) - TUBE_RADIUS, 50, 50);
+	glutSolidSphere(TUBE_RADIUS, 50, 50);
 	glPopMatrix();
 
 	glPopMatrix();
@@ -158,7 +155,7 @@ void drawPipes() {
 
 void drawBase() {
 	glPushMatrix();
-	glTranslatef(0.0f, -(sphereDiameter / 2 + baseDistance + baseY / 2), 0.0f);
+	glTranslatef(0.0f, -(SPHERE_D / 2 + BASE_DISTANCE + BASE_Y / 2), 0.0f);
 
 	glEnable(GL_TEXTURE_2D);
 	glBindTexture(GL_TEXTURE_2D, woodTextureId);
@@ -171,50 +168,94 @@ void drawBase() {
 
 	//Up
 	glNormal3f(0.0, 1.0f, 0.0f);
-	glTexCoord2f(0.0f, 0.0f); glVertex3f(-baseX / 2, baseY / 2, baseZ / 2);
-	glTexCoord2f(1.0f, 0.0f); glVertex3f(baseX / 2, baseY / 2, baseZ / 2);
-	glTexCoord2f(1.0f, 1.0f); glVertex3f(baseX / 2, baseY / 2, -baseZ / 2);
-	glTexCoord2f(0.0f, 1.0f); glVertex3f(-baseX / 2, baseY / 2, -baseZ / 2);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3f(-BASE_X / 2, BASE_Y / 2, BASE_Z / 2);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3f(BASE_X / 2, BASE_Y / 2, BASE_Z / 2);
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3f(BASE_X / 2, BASE_Y / 2, -BASE_Z / 2);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex3f(-BASE_X / 2, BASE_Y / 2, -BASE_Z / 2);
 
 	//Bottom
 	glNormal3f(0.0, -1.0f, 0.0f);
-	glTexCoord2f(0.0f, 0.0f); glVertex3f(-baseX / 2, -baseY / 2, baseZ / 2);
-	glTexCoord2f(1.0f, 0.0f); glVertex3f(baseX / 2, -baseY / 2, baseZ / 2);
-	glTexCoord2f(1.0f, 1.0f); glVertex3f(baseX / 2, -baseY / 2, -baseZ / 2);
-	glTexCoord2f(0.0f, 1.0f); glVertex3f(-baseX / 2, -baseY / 2, -baseZ / 2);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3f(-BASE_X / 2, -BASE_Y / 2, BASE_Z / 2);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3f(BASE_X / 2, -BASE_Y / 2, BASE_Z / 2);
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3f(BASE_X / 2, -BASE_Y / 2, -BASE_Z / 2);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex3f(-BASE_X / 2, -BASE_Y / 2, -BASE_Z / 2);
 
 	//Left
 	glNormal3f(-1.0, 0.0f, 0.0f);
-	glTexCoord2f(0.0f, 0.0f); glVertex3f(-baseX / 2, -baseY / 2, -baseZ / 2);
-	glTexCoord2f(1.0f, 0.0f); glVertex3f(-baseX / 2, -baseY / 2, baseZ / 2);
-	glTexCoord2f(1.0f, 1.0f); glVertex3f(-baseX / 2, baseY / 2, baseZ / 2);
-	glTexCoord2f(0.0f, 1.0f); glVertex3f(-baseX / 2, baseY / 2, -baseZ / 2);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3f(-BASE_X / 2, -BASE_Y / 2, -BASE_Z / 2);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3f(-BASE_X / 2, -BASE_Y / 2, BASE_Z / 2);
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3f(-BASE_X / 2, BASE_Y / 2, BASE_Z / 2);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex3f(-BASE_X / 2, BASE_Y / 2, -BASE_Z / 2);
 
 	//Right
 	glNormal3f(-1.0, 0.0f, 0.0f);
-	glTexCoord2f(0.0f, 0.0f); glVertex3f(baseX / 2, -baseY / 2, -baseZ / 2);
-	glTexCoord2f(1.0f, 0.0f); glVertex3f(baseX / 2, -baseY / 2, baseZ / 2);
-	glTexCoord2f(1.0f, 1.0f); glVertex3f(baseX / 2, baseY / 2, baseZ / 2);
-	glTexCoord2f(0.0f, 1.0f); glVertex3f(baseX / 2, baseY / 2, -baseZ / 2);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3f(BASE_X / 2, -BASE_Y / 2, -BASE_Z / 2);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3f(BASE_X / 2, -BASE_Y / 2, BASE_Z / 2);
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3f(BASE_X / 2, BASE_Y / 2, BASE_Z / 2);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex3f(BASE_X / 2, BASE_Y / 2, -BASE_Z / 2);
 
 	//Front
 	glNormal3f(0.0, 0.0f, 1.0f);
-	glTexCoord2f(0.0f, 0.0f); glVertex3f(-baseX / 2, -baseY / 2, baseZ / 2);
-	glTexCoord2f(1.0f, 0.0f); glVertex3f(baseX / 2, -baseY / 2, baseZ / 2);
-	glTexCoord2f(1.0f, 1.0f); glVertex3f(baseX / 2, baseY / 2, baseZ / 2);
-	glTexCoord2f(0.0f, 1.0f); glVertex3f(-baseX / 2, baseY / 2, baseZ / 2);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3f(-BASE_X / 2, -BASE_Y / 2, BASE_Z / 2);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3f(BASE_X / 2, -BASE_Y / 2, BASE_Z / 2);
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3f(BASE_X / 2, BASE_Y / 2, BASE_Z / 2);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex3f(-BASE_X / 2, BASE_Y / 2, BASE_Z / 2);
 
 	//Rear
 	glNormal3f(0.0, 0.0f, 1.0f);
-	glTexCoord2f(0.0f, 0.0f); glVertex3f(-baseX / 2, -baseY / 2, -baseZ / 2);
-	glTexCoord2f(1.0f, 0.0f); glVertex3f(baseX / 2, -baseY / 2, -baseZ / 2);
-	glTexCoord2f(1.0f, 1.0f); glVertex3f(baseX / 2, baseY / 2, -baseZ / 2);
-	glTexCoord2f(0.0f, 1.0f); glVertex3f(-baseX / 2, baseY / 2, -baseZ / 2);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3f(-BASE_X / 2, -BASE_Y / 2, -BASE_Z / 2);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3f(BASE_X / 2, -BASE_Y / 2, -BASE_Z / 2);
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3f(BASE_X / 2, BASE_Y / 2, -BASE_Z / 2);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex3f(-BASE_X / 2, BASE_Y / 2, -BASE_Z / 2);
 
 	glEnd();
 
 	glDisable(GL_TEXTURE_2D);
 	glPopMatrix();
+}
+
+void drawAxios() {
+	glColor3f(1.0, 0.0, 0.0);
+	glBegin(GL_LINES);
+	glVertex3f(-800.0, 0.0, 0.0);
+	glVertex3f(800.0, 0.0, 0.0);
+	glEnd();
+
+	glColor3f(0.0, 1.0, 0.0);
+	glBegin(GL_LINES);
+	glVertex3f(0.0, -800.0, 0.0);
+	glVertex3f(0.0, 800.0, 0.0);
+	glEnd();
+
+	glColor3f(0.0, 0.0, 1.0);
+	glBegin(GL_LINES);
+	glVertex3f(0.0, 0.0, -800.0);
+	glVertex3f(0.0, 0.0, 800.0);
+	glEnd();
 }
 
 void displayFunc() {
@@ -224,7 +265,7 @@ void displayFunc() {
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	glTranslatef(-cameraX, -cameraY, -cameraZ);
+	glTranslatef(-CAMERA_X, -CAMERA_Y, -CAMERA_Z);
 	glRotatef(cameraRotationX, 1.0, 0.0, 0.0);
 	glRotatef(cameraRotationY, 0.0, 1.0, 0.0);
 	glRotatef(cameraRotationZ, 0.0, 0.0, 1.0);
@@ -247,9 +288,32 @@ void displayFunc() {
 	glMaterialfv(GL_FRONT, GL_SPECULAR, specularMaterial);
 	glMaterialfv(GL_FRONT, GL_SHININESS, shininessMaterial);
 
+	drawAxios();
 	drawBase();
-	drawPipes();
-	drawSpheres();
+	drawTubes();
+
+	//glPushMatrix();
+
+	//glTranslatef(0.0f, TUBE_Y - TUBE_RADIUS - BASE_DISTANCE - SPHERE_D / 2.0f, 0.0f);
+
+	//for (int i = 1; i <= 5; i++) {
+	//	glPushMatrix();
+
+	//	glTranslatef(-5 / 2.0f - SPHERE_D / 2.0f + i * SPHERE_D, 0.0f, 0.0f);
+
+	//	if (i == 1 && angle < 0 || i == 5 && angle > 0) {
+	//		drawWire(angle);
+	//		drawSphere(angle);
+	//	}
+	//	else {
+	//		drawWire(0);
+	//		drawSphere(0);
+	//	}
+
+	//	glPopMatrix();
+	//}
+
+	//glPopMatrix();
 
 	glutSwapBuffers();
 }
@@ -286,8 +350,8 @@ void specialFunc(int key, int x, int y) {
 
 void timerFunc(int value) {
 	float x = A * cos(omega * t + fi0);                                            // Координата, смещение груза от положения равновесия 
-	float c = sqrt(wireLength * wireLength + x * x);                               // Пользуясь теоремой косинусов
-	float cosA = (x * x + c * c - wireLength * wireLength) / (2 * x * wireLength);
+	float c = sqrt(WIRE_L * WIRE_L + x * x);                               // Пользуясь теоремой косинусов
+	float cosA = (x * x + c * c - WIRE_L * WIRE_L) / (2 * x * WIRE_L);
 	float angleA = cosA * (180 / M_PI);
 
 	angle = angleA;
